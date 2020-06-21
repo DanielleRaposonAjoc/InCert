343f906fcdafe9f0e706a657840f7ca71c2bad5d
DanielleRaposonAjoc <danielleajoc123@gmail.com>
2020-06-21 14:24:03 +0800

    place text in middle of file using alesandra font

--- pdf-lib.js
+++ pdf-lib.js
@@ -1,50 +1,54 @@
 const { degrees, PDFDocument, rgb, StandardFonts } = require('pdf-lib');
-//import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+const fontkit = require('@pdf-lib/fontkit');
 var fs = require('fs');
-// var download = require('download');
-
-// async function modifyPdf() {
-// }
 
 async function modifyPdf() {
+  const fontBytes = fs.readFileSync('Alesandra.ttf');
+  const existingPdfBytes = fs.readFileSync('certificate.pdf');
 
-const existingPdfBytes = fs.readFileSync('certificate.pdf');
- 
-// Load a PDFDocument from the existing PDF bytes
-const pdfDoc = await PDFDocument.load(existingPdfBytes)
- 
-// Embed the Helvetica font
-const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
- 
-// Get the first page of the document
-const pages = pdfDoc.getPages()
-const firstPage = pages[0]
- 
-// Get the width and height of the first page
-const { width, height } = firstPage.getSize()
- 
-// Draw a string of text diagonally across the first page
-firstPage.drawText('This text was added with JavaScript!', {
-  x: 5,
-  y: height / 2,
-  size: 50,
-  font: helveticaFont,
-  color: rgb(0.95, 0.1, 0.1),
-})
- 
- 
-// Serialize the PDFDocument to bytes (a Uint8Array)
-const pdfBytes = await pdfDoc.save()
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
 
-fs.writeFileSync('editedDoc.pdf', pdfBytes);
+  // Register the `fontkit` instance
+  pdfDoc.registerFontkit(fontkit)
+
+  // Embed our custom font in the document
+  const customFont = await pdfDoc.embedFont(fontBytes)
+
+  // Get the first page of the document
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+
+  // Get the width and height of the first page
+  const { width, height } = firstPage.getSize()
+
+  // Create a string of text and measure its width and height in our custom font
+const text = 'Danenosaurus Rex'
+const textSize = 50
+const textWidth = customFont.widthOfTextAtSize(text, textSize)
+const textHeight = customFont.heightAtSize(textSize)
+
+  // Draw a string of text diagonally across the first page
+  console.log('width of page is', width);
+  console.log('height of page is', height);
+  console.log('width of text is', textWidth);
+  console.log('height of text is', textHeight);
+
+  firstPage.drawText(text, {
+    x: (width-textWidth)/2,
+    y: (height-textHeight)/2+60,
+    size: textSize,
+    font: customFont,
+    color: rgb(0.95, 0.1, 0.1),
+  })
+
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save()
+
+  fs.writeFileSync('editedDoc.pdf', pdfBytes);
 
 };
 
 modifyPdf();
 
-//module.exports = modifyPdf;
-
-//width of name subtracted by the width divided by two
-
-
-//express 
